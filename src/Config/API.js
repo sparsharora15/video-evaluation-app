73d8dcda57163b1c8a9d7eb46dbed5574ae29458
Sparsh Arora <arorasparsh28@gmail.com>
2023-08-14 16:55:08 +0530

    full done

--- src/Config/API.js
+++ src/Config/API.js
@@ -1,13 +1,16 @@
-import {BASE_URL} from "./BASE_URL";
-import axios from "axios"
+import { BASE_URL } from "./BASE_URL";
+import axios from "axios";
 export function getVideoData() {
   return axios.get(`${BASE_URL}get_all_videos`);
 }
 export function uploadFile(data) {
-  return axios.post(`${BASE_URL}uploadFile`,data);
+  return axios.post(`${BASE_URL}uploadFile`, data);
 }
 export function getVideoWithSubtitle(id) {
   return axios.get(`${BASE_URL}get_video_with_subtitle/${id}`);
+}
+export function upldateSubtitles(data) {
+  return axios.put(`http://127.0.0.1:5000/upldateSubtitles`,data);
 }
 
 // module.export = getVideoData
--- src/components/Videos.vue
+++ src/components/Videos.vue
@@ -5,6 +5,7 @@
 
         <div v-if="!isLoading" class="flex flex-wrap mx-1  lg:-mx-4">
             <div v-if="videos.length > 0" class="xl:grid-cols-4 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid">
+                
                 <div v-for="(video, index) in videos" :key="index" @click="handleonclick(video)"
                     class="shadow-4xl my-1 px-1  relative lg:my-4 lg:px-4 ">
                     <i class="fa-solid fa-play absolute top-[50%] right-[50%] fa-2xl "></i>
@@ -29,23 +30,27 @@
         </div>
         <div v-if="videoName != null"
             class=" justify-center flex items-center lg:w-[70%] w-full p-8  fixed z-[100] bg-[rgb(0,0,0,0.9)] top-[50px]   ">
-            <video id="video" v-if="videoName != null" @play="handlePlay" controls @timeupdate="updateCurrentTime"
-                ref="videoPlayer" :src="videoName"></video>
-            <span v-if="videoName != null" @click="videoName = null" style="user-select: none;cursor: pointer; z-index: 100;"
+            <video id="video" v-if="videoName != null" autoplay controls @timeupdate="updateCurrentTime" ref="videoPlayer"
+                :src="videoName"></video>
+            <span v-if="videoName != null" @click="videoName = null"
+                style="user-select: none;cursor: pointer; z-index: 100;"
                 class="absolute top-[5px] right-[20px] text-3xl font-bold text-white">&times;</span>
-            <p id="subtitle" v-if="currentSubtitle && videoName != null"
-                class="absolute bottom-[100px] bg-[black] cursor-pointer text-white p-[8px]">
-                {{ currentSubtitle }}
+            <p id="subtitle" v-if="currentSubtitle && videoName != null && !isEditing"
+                class="flex items-center absolute bottom-[100px] bg-[black] cursor-pointer text-white p-[8px]">
+                <i @click="pauseVideo(currentSubtitle)" class="fa-solid fa-pen-to-square edit-button mr-2"></i>
+                {{ currentSubtitle.text }}
             </p>
-            <!-- <i  class="ml-1 fa-solid fa-circle-info"></i> -->
-            
+            <input v-else-if="isEditing" type="text" v-model="currentSubtitle.text"
+                @change="handleChnageSubtitle(videoName)"
+                class="flex items-center absolute bottom-[100px] bg-[black] cursor-pointer text-white p-[8px]">
+
         </div>
         <Modal @refresh="getVideos" @loading="updateLoader($event)" />
 
     </div>
 </template>
 <script>
-import { getVideoData, getVideoWithSubtitle } from '@/Config/API'
+import { getVideoData, getVideoWithSubtitle, upldateSubtitles } from '@/Config/API'
 import { BASE_URL } from '@/Config/BASE_URL'
 import Heading from './Heading.vue'
 import moment from "moment";
@@ -65,16 +70,25 @@ export default {
             base_url: BASE_URL,
             videoName: null,
             subtitles: null,
+            videoData: {},
             currentTime: 0,
-            currentSubtitle: null,
+            currentSubtitle: {},
             isLoading: false,
-            // isLoading: false
+            data: this.subtitles,
+            isEditing: false,
+            editedSubtitle: null
         }
     },
     mounted() {
         this.getVideos()
         console.log(this.videos)
+        // console.log(this.currentSubtitle)
+
+
     },
+    // updated() {
+
+    // },
     methods: {
 
         handleonclick(el) {
@@ -82,13 +96,32 @@ export default {
             getVideoWithSubtitle(el._id.$oid).then((resp) => {
                 if (resp.data.statusCode === 200) {
                     // console.log(resp.data.data.subtitles.subtitles.subtitles)
-                    this.subtitles =resp.data.data.subtitles.subtitles.subtitles,
+                    this.subtitles = resp.data.data.subtitles.subtitles.subtitles,
                         this.videoName = resp.data.data.video
+                    this.videoData = resp.data.data
                 }
 
             })
                 .catch((err) => console.log(err))
 
+        },
+        handleChnageSubtitle() {
+            // console.log()
+            let video = document.getElementById('video')
+            video.play()
+            this.isEditing = !this.isEditing
+            console.log(this.videoData._id)
+            console.log(this.subtitles)
+            // console.log(first)
+            // const 
+            const finalData = { data: {subtitles:{subtitles:this.subtitles}}, videoId: this.videoData._id }
+            upldateSubtitles(finalData).then().catch((err) => console.log(err))
+
+        },
+        pauseVideo() {
+            let video = document.getElementById('video')
+            video.pause()
+            this.isEditing = true
         },
         updateLoader(Loading) {
             this.isLoading = Loading;
@@ -104,7 +137,7 @@ export default {
                 })
                 .catch((err) => console.log(err))
         },
-        updateCurrentTime() { 
+        updateCurrentTime() {
             var videoElement = document.getElementById('video');
             if (videoElement) {
                 const duration = moment.duration(videoElement.currentTime, 'seconds');
@@ -122,8 +155,7 @@ export default {
                 const subtitle = this.subtitles.find(sub => this.currentTime >= sub.timeStampFrom && this.currentTime <= sub.timeStampTo);
 
                 if (subtitle) {
-                    console.log(subtitle.text)
-                    this.currentSubtitle = subtitle.text;
+                    this.currentSubtitle = subtitle;
                 } else {
                     this.currentSubtitle = '';
                 }
@@ -134,3 +166,12 @@ export default {
 
 }
 </script>
+<style>
+.edit-button {
+    display: none;
+}
+
+#subtitle:hover .edit-button {
+    display: block;
+}
+</style>
