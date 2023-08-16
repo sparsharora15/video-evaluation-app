a4ad5e850ed2abfd6193e053a0eb339864ea48de
Sparsh Arora <arorasparsh28@gmail.com>
2023-08-16 10:20:08 +0530

    main

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -30,7 +30,7 @@
                     </p>
                     <label class="mt-4 block mb-2 text-sm font-medium text-gray-900 dark:text-white" for="file_input">
                         Upload subtitles (in json format)
-                        <a :href="`${base_url}download/Example.json`">
+                        <a :href="`${base_url}uploads/Example.json`">
                             <i data-tooltip-target="tooltip-default" class="ml-1 fa-solid fa-circle-info"></i>
                             <div id="tooltip-default" role="tooltip"
                                 class="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700">
