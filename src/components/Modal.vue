d8a3fd91ea424ff4ed4a06aad9b10a23fed22bc2
Sparsh Arora <arorasparsh28@gmail.com>
2023-08-16 10:29:40 +0530

    changes in download url

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -34,7 +34,7 @@
                             <i data-tooltip-target="tooltip-default" class="ml-1 fa-solid fa-circle-info"></i>
                             <div id="tooltip-default" role="tooltip"
                                 class="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700">
-                                click to download example Json file
+                                click to see example Json file
                                 <div class="tooltip-arrow" data-popper-arrow></div>
                             </div>
                         </a>
